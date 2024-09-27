cb0d285ac047cc1eff3949d4c759302e9f21a530
shadab khan <shadabkhan@shadabs-MBP.lan>
2024-09-27 15:56:52 +0530

    change anchor tag to Link tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
-
-import { FaSearch, FaUser } from 'react-icons/fa'; // Import icons
-import { Link } from 'react-router-dom';
-import  FlyoutCart  from './FlyoutCart';
+import { FaSearch, FaUser } from "react-icons/fa"; // Import icons
+import { Link } from "react-router-dom";
+import FlyoutCart from "./FlyoutCart";
 
 const Header = () => {
   return (
@@ -9,28 +8,42 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo Section */}
         <div className="text-2xl font-bold">
-          <Link to ="/">3legant.</Link>
+          <Link to="/">3legant.</Link>
         </div>
 
         {/* Navigation Links */}
         <nav className="hidden md:flex space-x-6 text-gray-600">
-          <Link to="/" className="hover:text-black transition duration-300">Home</Link>
-          <Link to="/shop" className="hover:text-black transition duration-300">Shop</Link>
-          <Link to="/products" className="hover:text-black transition duration-300">Product</Link>
-          <Link to="/contact-us" className="hover:text-black transition duration-300">Contact Us</Link>
+          <Link to="/" className="hover:text-black transition duration-300">
+            Home
+          </Link>
+          <Link to="/shop" className="hover:text-black transition duration-300">
+            Shop
+          </Link>
+          <Link
+            to="/products"
+            className="hover:text-black transition duration-300"
+          >
+            Product
+          </Link>
+          <Link
+            to="/contact-us"
+            className="hover:text-black transition duration-300"
+          >
+            Contact Us
+          </Link>
         </nav>
 
         {/* Icons Section */}
         <div className="flex space-x-6 text-gray-600">
-          <a href="#" className="hover:text-black transition duration-300">
+          <Link href="#" className="hover:text-black transition duration-300">
             <FaSearch size={20} />
-          </a>
-          <a href="#" className="hover:text-black transition duration-300">
-            <FlyoutCart/>
-          </a>
-          <a href="#" className="hover:text-black transition duration-300">
+          </Link>
+          <Link className="hover:text-black transition duration-300">
+            <FlyoutCart />
+          </Link>
+          <Link href="#" className="hover:text-black transition duration-300">
             <FaUser size={20} />
-          </a>
+          </Link>
         </div>
       </div>
     </header>
